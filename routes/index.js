ebfcec9b5c91ad4642644f0ce44949e830328ebe
martinhadid <martinhadid@gmail.com>
2020-04-22 11:57:23 +0300

    create entry points

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const podcastController = require("../controllers/podcastController");
+const ratingController = require("../controllers/ratingController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
 router.get("/", podcastController.home);
 router.post("/add", catchErrors(podcastController.createPodcast));
+
+router.get("/rating", ratingController.rate);
+router.post("/thankyou", catchErrors(ratingController.thankyou));
 
 module.exports = router;
